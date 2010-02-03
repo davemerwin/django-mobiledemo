5337fff4f1744a7db01e452b59931864abfcc3c1
Dave Merwin <dave@davemerwin.com>
2010-02-02 17:14:00 -0800

    installing jqtouch mobile files and tweaking base.html

---
+++ media/js/mobile.js
@@ -0,0 +1,18 @@
+var jQT = new $.jQTouch({
+  icon:'touch-icon.png',
+  //addGlossToIcon: false,
+  startupScreen:"startup-screen.png",
+  statusBar:'black-translucent',
+  preloadImages:[
+    '/media/themes/apple/img/back_button.png',
+    '/media/themes/apple/img/back_button_clicked.png',
+    '/media/themes/apple/img/button_clicked.png',
+    '/media/themes/apple/img/grayButton.png',
+    '/media/themes/apple/img/whiteButton.png',
+    '/media/themes/apple/img/loading.gif'
+  ]
+});
+
+jQuery(function() {
+  // Add custom handler code here.
+});
